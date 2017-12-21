89ed8897b363ca5d410c7ae93729e2fd6488dae1
Ben Davidson <ben@bendavidson.me>
2017-12-21 16:55:48 -0500

    Added placeholder content for homepage layout

--- partials/media-capabilityStack.php
+++ partials/media-capabilityStack.php
@@ -1,1 +1,62 @@
-<?php // Stacked media blocks for E&E capabilities, with introductory content
+<?php // Stacked media blocks for E&E capabilities, with introductory content ?>
+<div class="o-capabilityStack">
+  <h2 class="o-capabilityStack__headline">
+    Our Capabilities
+  </h2>
+  <img class="o-capabilityStack__topDecoration" src="http://via.placeholder.com/32x64" />
+  <div class="o-capabilityStack__description">
+    Vestibulum sed nunc enim. Nullam semper imperdiet magna, ut mattis tortor gravida sit amet. Integer sagittis finibus enim, id tempus purus vulputate vel.
+  </div>
+
+  <div class="o-capabilityStack__stack">
+
+    <div class="m-capabilityRow">
+      <img class="m-capabilityRow__image" src="http://via.placeholder.com/1591x741?text=capability" />
+      <div class="a-rowLabel m-capabilityRow__label">
+        <h3 class="a-rowLabel__title">
+          Tooling&amp; Stamping
+        </h3>
+        <svg class="a-rowLabel__flag">
+          <path fill="#FF0000" d="M 0,0 L 0,64 L 64,64" />
+        </svg>
+      </div>
+    </div>
+
+    <div class="m-capabilityRow">
+      <img class="m-capabilityRow__image" src="http://via.placeholder.com/1591x741?text=capability" />
+      <div class="a-rowLabel m-capabilityRow__label">
+        <h3 class="a-rowLabel__title">
+          Welded Component &amp; Complete System
+        </h3>
+        <svg class="a-rowLabel__flag">
+          <path fill="#FF0000" d="M 0,0 L 0,64 L 64,64" />
+        </svg>
+      </div>
+    </div>
+
+    <div class="m-capabilityRow">
+      <img class="m-capabilityRow__image" src="http://via.placeholder.com/1591x741?text=capability" />
+      <div class="a-rowLabel m-capabilityRow__label">
+        <h3 class="a-rowLabel__title">
+          Roll Forming
+        </h3>
+        <svg class="a-rowLabel__flag">
+          <path fill="#FF0000" d="M 0,0 L 0,64 L 64,64" />
+        </svg>
+      </div>
+    </div>
+
+    <div class="m-capabilityRow">
+      <img class="m-capabilityRow__image" src="http://via.placeholder.com/1591x741?text=capability" />
+      <div class="a-rowLabel m-capabilityRow__label">
+        <h3 class="a-rowLabel__title">
+          Fastening
+        </h3>
+        <svg class="a-rowLabel__flag">
+          <path fill="#FF0000" d="M 0,0 L 0,64 L 64,64" />
+        </svg>
+      </div>
+    </div>
+
+  </div>
+</div>
--- partials/media-marketingBlocks.php
+++ partials/media-marketingBlocks.php
@@ -1,1 +1,49 @@
-<?php // Blocks of broad information used for marketing purposes
+<?php // Blocks of broad information used for marketing purposes ?>
+<div class="o-marketingBlocks">
+  <div class="o-marketingBlocks__description">
+    Maecenas quis diam aliquet, auctor quam auctor Vestibulum sed nunc enim. Nullam semper imperdiet magna, ut mattis tortor gravida sit amet. Integer.
+  </div>
+
+  <div class="o-marketingBlocks__block">
+    <div class="m-marketingBlock">
+      <h3 class="m-marketingBlock__headline">
+        50+ Years
+      </h3>
+      <h4 class="m-marketingBlock__body">
+        experience and diversity in talent
+      </h4>
+      <svg class="m-marketingBlock__decoration">
+        <path fill="none" stroke="yellow" stroke-width="4" d="M 0,32 L 32,0 L 64,32" />
+      </svg>
+    </div>
+  </div>
+
+  <div class="o-marketingBlocks__block">
+    <div class="m-marketingBlock">
+      <h3 class="m-marketingBlock__headline">
+        50+ Years
+      </h3>
+      <h4 class="m-marketingBlock__body">
+        experience and diversity in talent
+      </h4>
+      <svg class="m-marketingBlock__decoration">
+        <path fill="none" stroke="yellow" stroke-width="4" d="M 0,32 L 32,0 L 64,32" />
+      </svg>
+    </div>
+  </div>
+
+  <div class="o-marketingBlocks__block">
+    <div class="m-marketingBlock">
+      <h3 class="m-marketingBlock__headline">
+        50+ Years
+      </h3>
+      <h4 class="m-marketingBlock__body">
+        experience and diversity in talent
+      </h4>
+      <svg class="m-marketingBlock__decoration">
+        <path fill="none" stroke="yellow" stroke-width="4" d="M 0,32 L 32,0 L 64,32" />
+      </svg>
+    </div>
+  </div>
+
+</div>
