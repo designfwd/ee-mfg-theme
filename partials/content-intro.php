831e5d2fbb668a75ffc1458c691b2f1c52776cb4
Ben Davidson <ben@bendavidson.me>
2018-01-05 12:26:14 -0500

    Updated category landing page layout styles

--- partials/content-intro.php
+++ partials/content-intro.php
@@ -1,10 +1,14 @@
 <?php // Introductory paragraph content ?>
 <div class="o-introContent">
-  <img class="o-introContent__decoration" src="http://via.placeholder.com/70x60?text=decoration" />
-  <h1 class="o-introContent__title">
-    Intro content title
-  </h1>
-  <div class="o-introContent__paragraph">
-    Maecenas quis diam aliquet, auctor quam auctor Vestibulum sed nunc enim. Nullam semper imperdiet magna, ut mattis tortor gravida sit amet. Integer sagittis finibus enim, id tempus purus vulputate vel.
+  <div class="o-introContent__before">
+    <img class="o-introContent__decoration" src="http://via.placeholder.com/70x90?text=decoration" />
+  </div>
+  <div class="o-introContent__content">
+    <h1 class="o-introContent__title">
+      Intro content title
+    </h1>
+    <div class="o-introContent__paragraph">
+      Maecenas quis diam aliquet, auctor quam auctor Vestibulum sed nunc enim. Nullam semper imperdiet magna, ut mattis tortor gravida sit amet. Integer sagittis finibus enim, id tempus purus vulputate vel.
+    </div>
   </div>
 </div>
--- partials/media-capabilityBlocks.php
+++ partials/media-capabilityBlocks.php
@@ -1,74 +1,88 @@
 <?php // Alternating media blocks, for displaying information about capabilties ?>
 <div class="o-capabilityBlocks">
+
   <div class="o-capabilityBlocks__block">
-
     <div class="m-capabilityBlock">
       <p class="m-capabilityBlock__description">
         Vestibulum sed nunc enim. Nullam semper imperdiet magna, ut mattis tortor gravida sit amet. Integer sagittis finibus enim, id tempus purus vulputate vel.
       </p>
-      <div class="m-capabilityBlock__mediaBlock">
+      <a class="m-capabilityBlock__mediaBlock" href="#">
         <img class="m-capabilityBlock__image" src="http://via.placeholder.com/1591x741?text=capability" />
-        <div class="a-blockLabel m-capabilityBlock__label">
-          <h2 class="a-blockLabel__title">
-            Tooling &amp; Stamping
-          </h2>
-          <svg class="a-blockLabel__flag">
-            <path fill="#FF0000" d="M 0,0 L 64,64 L 0,64" />
-          </svg>
+        <div class="m-capabilityBlock__label">
+          <div class="a-blockLabel">
+            <h2 class="a-blockLabel__title">
+              Tooling &amp; Stamping
+            </h2>
+            <svg class="a-blockLabel__flag" viewBox="0 0 64 64">
+              <path fill="#FF0000" d="M 0,0 L 64,64 L 0,64" />
+            </svg>
+          </div>
         </div>
-      </div>
+      </a>
     </div>
-
-    <div class="m-capabilityBlock m-capabilityBlock--reverse">
-      <p class="m-capabilityBlock__description">
-        Vestibulum sed nunc enim. Nullam semper imperdiet magna, ut mattis tortor gravida sit amet. Integer sagittis finibus enim, id tempus purus vulputate vel.
-      </p>
-      <div class="m-capabilityBlock__mediaBlock">
-        <img class="m-capabilityBlock__image" src="http://via.placeholder.com/1591x741?text=capability" />
-        <div class="a-blockLabel a-blockLabel--reverse m-capabilityBlock__label">
-          <h2 class="a-blockLabel__title">
-            Welded Component &amp; Complete System
-          </h2>
-          <svg class="a-blockLabel__flag a-blockLabel__flag--reverse" style="transform: scaleX(-1);">
-            <path fill="#FF0000" d="M 0,0 L 64,64 L 0,64" />
-          </svg>
-        </div>
-      </div>
-    </div>
-
-    <div class="m-capabilityBlock">
-      <p class="m-capabilityBlock__description">
-        Vestibulum sed nunc enim. Nullam semper imperdiet magna, ut mattis tortor gravida sit amet. Integer sagittis finibus enim, id tempus purus vulputate vel.
-      </p>
-      <div class="m-capabilityBlock__mediaBlock">
-        <img class="m-capabilityBlock__image" src="http://via.placeholder.com/1591x741?text=capability" />
-        <div class="a-blockLabel a-blockLabel--reverse m-capabilityBlock__label">
-          <h2 class="a-blockLabel__title">
-            Roll Forming
-          </h2>
-          <svg class="a-blockLabel__flag">
-            <path fill="#FF0000" d="M 0,0 L 64,64 L 0,64" />
-          </svg>
-        </div>
-      </div>
-    </div>
-
-    <div class="m-capabilityBlock m-capabilityBlock--reverse">
-      <p class="m-capabilityBlock__description">
-        Vestibulum sed nunc enim. Nullam semper imperdiet magna, ut mattis tortor gravida sit amet. Integer sagittis finibus enim, id tempus purus vulputate vel.
-      </p>
-      <div class="m-capabilityBlock__mediaBlock">
-        <img class="m-capabilityBlock__image" src="http://via.placeholder.com/1591x741?text=capability" />
-        <div class="a-blockLabel a-blockLabel--reverse m-capabilityBlock__label">
-          <h2 class="a-blockLabel__title">
-            Fastening
-          </h2>
-          <svg class="a-blockLabel__flag a-blockLabel__flag--reverse" style="transform: scaleX(-1);">
-            <path fill="#FF0000" d="M 0,0 L 64,64 L 0,64" />
-          </svg>
-        </div>
-      </div>
-    </div>
-
   </div>
+
+  <div class="o-capabilityBlocks__block o-capabilityBlocks__block--reverse">
+    <div class="m-capabilityBlock m-capabilityBlock--reverse">
+      <a class="m-capabilityBlock__mediaBlock" href="#">
+        <img class="m-capabilityBlock__image m-capabilityBlock__image--reverse" src="http://via.placeholder.com/1591x741?text=capability" />
+        <div class="m-capabilityBlock__label m-capabilityBlock__label--reverse">
+          <div class="a-blockLabel a-blockLabel--reverse">
+            <h2 class="a-blockLabel__title">
+              Welded Component &amp; Complete System
+            </h2>
+            <svg class="a-blockLabel__flag a-blockLabel__flag--reverse" viewBox="0 0 64 64">
+              <path fill="#FF0000" d="M 0,0 L 64,64 L 0,64" />
+            </svg>
+          </div>
+        </div>
+      </a>
+      <p class="m-capabilityBlock__description">
+        Vestibulum sed nunc enim. Nullam semper imperdiet magna, ut mattis tortor gravida sit amet. Integer sagittis finibus enim, id tempus purus vulputate vel.
+      </p>
+    </div>
+  </div>
+
+  <div class="o-capabilityBlocks__block">
+    <div class="m-capabilityBlock">
+      <p class="m-capabilityBlock__description">
+        Vestibulum sed nunc enim. Nullam semper imperdiet magna, ut mattis tortor gravida sit amet. Integer sagittis finibus enim, id tempus purus vulputate vel.
+      </p>
+      <a class="m-capabilityBlock__mediaBlock" href="#">
+        <img class="m-capabilityBlock__image" src="http://via.placeholder.com/1591x741?text=capability" />
+        <div class="m-capabilityBlock__label">
+          <div class="a-blockLabel">
+            <h2 class="a-blockLabel__title">
+              Roll Forming
+            </h2>
+            <svg class="a-blockLabel__flag" viewBox="0 0 64 64">
+              <path fill="#FF0000" d="M 0,0 L 64,64 L 0,64" />
+            </svg>
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+
+  <div class="o-capabilityBlocks__block o-capabilityBlocks__block--reverse">
+    <div class="m-capabilityBlock m-capabilityBlock--reverse">
+      <a class="m-capabilityBlock__mediaBlock" href="#">
+        <img class="m-capabilityBlock__image m-capabilityBlock__image--reverse" src="http://via.placeholder.com/1591x741?text=capability" />
+        <div class="m-capabilityBlock__label m-capabilityBlock__label--reverse">
+          <div class="a-blockLabel a-blockLabel--reverse">
+            <h2 class="a-blockLabel__title">
+              Fastening
+            </h2>
+            <svg class="a-blockLabel__flag a-blockLabel__flag--reverse" viewBox="0 0 64 64">
+              <path fill="#FF0000" d="M 0,0 L 64,64 L 0,64" />
+            </svg>
+          </div>
+        </div>
+      </a>
+      <p class="m-capabilityBlock__description">
+        Vestibulum sed nunc enim. Nullam semper imperdiet magna, ut mattis tortor gravida sit amet. Integer sagittis finibus enim, id tempus purus vulputate vel.
+      </p>
+    </div>
+  </div>
+
 </div>
