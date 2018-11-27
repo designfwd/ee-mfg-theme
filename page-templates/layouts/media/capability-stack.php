f16024131c78790cc4e746c24b405c096847cf67
melderro <melanie@designfwd.com>
2018-11-27 17:42:53 -0500

    Getting back on track changes
    styles

--- page-templates/layouts/media/capability-stack.php
+++ page-templates/layouts/media/capability-stack.php
@@ -1,7 +1,7 @@
 <?php // Stacked media blocks for E&E capabilities, with introductory content ?>
 <div class="o-capabilityStack">
   <h2 class="o-capabilityStack__headline">
-    Our Capabilities
+  <?php the_field('capabilities_title'); ?>
     <svg class="o-capabilityStack__topDecoration" viewBox="0 0 100 100">
       <path fill="none" stroke="black" stroke-width="8" d="M 0,0 L 30,25 L 60,0" />
       <path fill="none" stroke="black" stroke-width="8" d="M 0,30 L 30,55 L 60,30" />
