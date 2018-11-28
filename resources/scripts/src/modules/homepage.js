906ee596abfa0da5bc39c6439910dff606a57026
sajison <saji@designfwd.com>
2018-11-27 23:45:34 -0500

    search, case study, homepage slider

--- resources/scripts/src/modules/homepage.js
+++ resources/scripts/src/modules/homepage.js
@@ -1,10 +1,18 @@
 import $ from 'jquery';
 import '../vendor/slick-carousel';
     
-   $('.o-featureSlider').slick({
-        lazyLoad: 'ondemand',
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-      });
+  $('.o-featureSlider').slick({
+    lazyLoad: 'ondemand',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  $('.o-industrySlider').slick({
+    lazyLoad: 'ondemand',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
