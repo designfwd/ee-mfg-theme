e6ec9ac1ed9bc4a8f232665decc374ed341545a5
melderro <melanie@designfwd.com>
2018-11-29 15:47:08 -0500

    Media Query styles, forms

--- page-templates/layouts/slider/solution-capabilities.php
+++ page-templates/layouts/slider/solution-capabilities.php
@@ -1,12 +1,15 @@
+<h2 class="o-capabilitiesBlock__title">Capabilities
+  <svg class="o-capabilitiesBlock__topDecoration" viewBox="0 0 100 100">
+    <path fill="none" stroke="black" stroke-width="8" d="M 0,0 L 30,25 L 60,0" />
+    <path fill="none" stroke="black" stroke-width="8" d="M 0,30 L 30,55 L 60,30" />
+    <path fill="none" stroke="black" stroke-width="8" d="M 0,60 L 30,85 L 60,60" />
+  </svg>
+</h2>
 <?php // Solution-specific capabilities shown in a media block ?>
 <div class="o-capabilitiesBlock o-capabilitesBlock--solution">    
 <div class="o-capabilitiesBlock__block"> 
   <img class="o-capabilitesBlock__image" src="<?php echo get_acf_image( get_field('solutions_capabilities_background_image'), '2560w', 'http://via.placeholder.com/2677x1271?text=call+to+action+background'); ?>" />   
   <div class="o-capabilitiesSlider__wrapper"> 
-    <svg class="a-rowLabel__flag" viewBox="0 0 64 64">
-      <path fill="#FF0000" d="M 0,0 L 0,64 L 64,64" />
-    </svg>
-    <h2 class="o-supplementaryContent__title">Capabilities</h2>
   <div class="o-capabilitiesSlider"> 
     <?php
       if( have_rows('solutions_capabilities_slideshow') ):
