01da567bb45fbe72291cc7d7de1b95d9e22a59a9
melderro <melanie@designfwd.com>
2019-02-19 17:11:32 -0500

    history, intro content and overall general edits

---
+++ page-templates/layouts/media/history-video-content.php
@@ -0,0 +1,19 @@
+<?php // Stacked media blocks for E&E capabilities, with introductory content ?>
+<div class="o-videoContent">
+  <h2 class="o-videoContent__headline">
+    <?php the_field('history_video_content_title'); ?>
+      <svg class="o-videoContent__topDecoration" viewBox="0 0 100 100">
+        <path fill="none" stroke="black" stroke-width="8" d="M 0,0 L 30,25 L 60,0" />
+        <path fill="none" stroke="black" stroke-width="8" d="M 0,30 L 30,55 L 60,30" />
+        <path fill="none" stroke="black" stroke-width="8" d="M 0,60 L 30,85 L 60,60" />
+      </svg>
+  </h2>
+  <div class="o-videoContent__container"> 
+    <div class="o-videoContent__description">
+      <?php the_field('history_video_content_description'); ?>
+    </div>
+  </div>
+  <div class="o-videoContent__video">
+    <?php the_field('history_video_content_video'); ?>
+  </div>
+</div>
