8bd8b14025d81ca33dec8cca3ea18c941cc6e2e7
melderro <melanie@designfwd.com>
2019-06-25 15:02:32 -0400

    6-25-19 morning edits

--- page-templates/layouts/links/category.php
+++ page-templates/layouts/links/category.php
@@ -5,7 +5,7 @@
   if(have_rows('general_supplementary_links','option')):
     while(have_rows('general_supplementary_links', 'option')): the_row();
 ?>
-  <a class="o-categoryLinks__link" href="<?php the_sub_field('general_supplementary_link_title', 'option'); ?>">
+  <a class="o-categoryLinks__link" href="<?php the_sub_field('general_supplementary_link_link', 'option'); ?>">
     <div class="m-categoryLinks">
       <img class="m-categoryLinks__image lazyload" sizes="(min-width: 900px)860px, (min-width: 600px)600px" srcset="<?php echo get_acf_image_srcset(get_sub_field('general_supplementary_link_image')); ?>" data-src="<?php echo get_acf_image( get_sub_field('general_supplementary_link_image', 'option'), '960w', 'http://via.placeholder.com/934x687?text=image'); ?> " />
       <h3 class="o-categoryLinks__title">
--- page-templates/layouts/media/linked-block.php
+++ page-templates/layouts/media/linked-block.php
@@ -1,4 +1,3 @@
 <?php // Linked media block for showcasing a category or other information area ?>
 <div class="o-linkedBlock">
-  PLACEHOLDER FOR LINKED MEDIA BLOCK AREA
 </div>
